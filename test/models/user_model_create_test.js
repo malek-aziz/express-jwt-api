1c98677d024317838263b32eadc65640fa46a3f5
Rizwan Saleem <rizi.saleem23@gmail.com>
2019-08-30 01:11:43 +0700

    init project

---
+++ test/models/user_model_create_test.js
@@ -0,0 +1,17 @@
+
+let UserModel = require('../../models/UserModel');
+
+let user = new UserModel({
+    name: 'Trần Phú Quy',
+    username: 'tranphuquy19',
+    email: 'tranphuquy19@gmail.com',
+    password: 'root@123',
+    admin: true
+});
+user.save().then(mess => {
+    console.log(mess);
+}).catch(err => {
+    console.log(err);
+});
+
+// MONGODB_HOSTNAME=cluster0-zyqpm.gcp.mongodb.net MONGODB_DATABASENAME=music-tv-api MONGODB_USERNAME=tranphuquy19 MONGODB_PASSWORD=root@123 node user_model_create_test.js
