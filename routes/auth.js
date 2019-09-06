650c2ab8a465a9ed089fbb21e9c50e184389deac
Rizwan Saleem <rizi.saleem23@gmail.com>
2019-09-07 02:50:26 +0700

    clean code

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+
 var passport = require('passport');
 var passportConf = require('../app/middleware/passport');
 
+var middlewareLogin = passport.authenticate('local', { session: false });
 
 var userController = require('../app/controllers/UsersController');
 
-router.post('/login', passport.authenticate('local', { session: false }), userController.login);
+router.post('/login', middlewareLogin, userController.login);
 router.get('/google-auth-callback', userController.ggAuth);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 var userController = require('../app/controllers/UsersController');
-var checkAuth = require('../app/middleware/check-auth');
+
 var passport = require('passport');
 var passportConf = require('../app/middleware/passport');
 
+var middlewareJWT = passport.authenticate('jwt', { session: false });
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.route('/users')
-  .get(passport.authenticate('jwt', {session: false}), userController.index)
+  .get(middlewareJWT, userController.index)
   .post(userController.new);
 
 router.route('/users/:userId')
   .get(userController.view)
-  .patch(passport.authenticate('jwt', {session: false}), userController.update)
-  .put(passport.authenticate('jwt', {session: false}), userController.update)
-  .delete(passport.authenticate('jwt', {session: false}), userController.delete);
+  .patch(middlewareJWT, userController.update)
+  .put(middlewareJWT, userController.update)
+  .delete(middlewareJWT, userController.delete);
 
 module.exports = router;
