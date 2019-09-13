33b6b07795b3285c5d53e4aab0b177ad5bc2018a
Rizwan Saleem <rizi.saleem23@gmail.com>
2019-09-13 23:58:58 +0700

    access control ABAC (Attribute-Based Access Control)

---
+++ app/helpers/accesscontrol.js
@@ -0,0 +1,17 @@
+var AccessControl = require('accesscontrol');
+var Role = require('../models/RoleModel');
+
+async function accessControl() {
+    const grantList = await Role.find({});
+    const filteredObj = grantList.map(e => ({
+        role: e.role,
+        resource: e.resource,
+        action: e.action,
+        attributes: e.attributes
+    }));
+    const ac = new AccessControl();
+    ac.setGrants(filteredObj);
+    return ac;
+}
+
+module.exports = accessControl;
